64dc1e45a7e21e5ec13a0f50cd19071aff76cdc3
Robin Malhotra <me@rmalhotra.com>
2019-10-06 02:45:31 +0530

    Trying out doctests!
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

---
+++ chap5/lib/chap5.ex
@@ -0,0 +1,34 @@
+defmodule Chap5 do
+  @moduledoc """
+  Documentation for Chap5.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Chap5.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  @doc """
+  iex> Chap5.add(39,3)
+  42
+  """
+  def add(a, b) do
+    a + b
+  end
+
+  @doc """
+  iex> Chap5.list_concat([:a, :b], [:c, :d])
+  [:a, :b, :c, :d]
+  """
+  def list_concat(a, b) do
+    a ++ b
+  end
+end
