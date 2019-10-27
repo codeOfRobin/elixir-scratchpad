34d3afadde25555516ba65a79fe0ed0dcca583d4
Robin Malhotra <me@rmalhotra.com>
2019-10-28 02:41:51 +0530

    split and take still left
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

--- chap9/lib/chap9.ex
+++ chap9/lib/chap9.ex
@@ -16,3 +16,54 @@ defmodule Chap9 do
     :world
   end
 end
+
+defmodule MyEnum do
+  @doc """
+  iex> MyEnum.all?([2,4,6,8], fn x -> rem(x,2) == 0 end)
+  true
+  iex> MyEnum.all?([1,2,3,4], fn x -> rem(x,2) == 0 end)
+  false
+  """
+
+  def all?([], _filterFunc), do: true
+
+  def all?(sequence, filterFunc) do
+    [head | tail] = sequence
+
+    case filterFunc.(head) do
+      false -> false
+      true -> all?(tail, filterFunc)
+    end
+  end
+
+  def each(sequence, closure) do
+    case sequence do
+      [head | tail] ->
+        closure.(head)
+        each(tail, closure)
+
+      [] ->
+        :ok
+    end
+  end
+
+  def reduce(sequence, initial, closure) do
+    case sequence do
+      [] ->
+        initial
+
+      [head | tail] ->
+        reduce(tail, closure.(initial, head), closure)
+    end
+  end
+
+  def filter(sequence, closure) do
+    reduce(sequence, [], fn result, element ->
+      if closure.(element) do
+        result ++ [element]
+      else
+        result
+      end
+    end)
+  end
+end
--- chap9/test/chap9_test.exs
+++ chap9/test/chap9_test.exs
@@ -1,8 +1,21 @@
 defmodule Chap9Test do
   use ExUnit.Case
   doctest Chap9
+  doctest MyEnum
 
   test "greets the world" do
     assert Chap9.hello() == :world
+  end
+
+  test "our reduce works just like the others" do
+    additionAccumulator = fn acc, element -> element + acc end
+    seq = [1, 2, 3, 4, 5, 6]
+    assert MyEnum.reduce(seq, 0, additionAccumulator) == Enum.reduce(seq, 0, additionAccumulator)
+  end
+
+  test "our filter" do
+    filterCondition = fn element -> rem(element, 2) == 0 end
+    seq = [1, 2, 3, 4, 5, 6]
+    assert MyEnum.filter(seq, filterCondition) == Enum.filter(seq, filterCondition)
   end
 end
