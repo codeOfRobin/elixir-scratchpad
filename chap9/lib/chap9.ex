24b7368c21a24276f3161b0e4911321be873b867
Robin Malhotra <me@rmalhotra.com>
2019-10-28 00:06:49 +0530

    Add project for chapter 9
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

---
+++ chap9/lib/chap9.ex
@@ -0,0 +1,18 @@
+defmodule Chap9 do
+  @moduledoc """
+  Documentation for Chap9.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Chap9.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
