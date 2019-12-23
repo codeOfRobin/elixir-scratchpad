02f532a370a740aa20b71007bea1547db91c5776
Robin Malhotra <me@rmalhotra.com>
2019-12-24 01:36:50 +0530

    Add calculator
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

--- chap11/lib/chap11.ex
+++ chap11/lib/chap11.ex
@@ -39,4 +39,47 @@ defmodule Chap11 do
   def anagram?(word1, word2) do
     Enum.empty?(word1 -- word2)
   end
+
+  _ = """
+  about
+  iex(2)> ['cat' | 'dog']
+  ['cat', 100, 111, 103]
+  the reason is - elixir array syntax is [head | tail] where tail is the rest of the list! so rather than being treated as a series of elements rather than one element
+  """
+
+  def calculateExpression(number1, ?+, number2), do: number1 + number2
+
+  def calculateExpression(number1, ?-, number2), do: number1 - number2
+
+  def calculateExpression(number1, ?*, number2), do: number1 * number2
+
+  def calculateExpression(number1, ?/, number2), do: number1 / number2
+
+  @doc """
+  iex> Chap11.parseDigits('123')
+  {123, ''}
+  """
+  def parseDigits(str), do: parseDigits(str, 0)
+
+  def parseDigits([head | tail], value) when head in '0123456789' do
+    parseDigits(tail, value * 10 + head - ?0)
+  end
+
+  def parseDigits(leftoverCharacters, value) do
+    {value, leftoverCharacters}
+  end
+
+  @doc """
+  iex> Chap11.calculate('123+27')
+  150
+  """
+  def calculate(string) do
+    {number1, [operator | tail]} =
+      string
+      |> parseDigits()
+
+    {number2, _} = tail |> parseDigits()
+
+    calculateExpression(number1, operator, number2)
+  end
 end
