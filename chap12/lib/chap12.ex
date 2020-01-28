0cecee6c41b76c033446a42159fd9e13ce250a00
Robin Malhotra <robinm@twitter.com>
2020-01-28 14:53:03 +0000

    Done with chapter 12

--- chap12/lib/chap12.ex
+++ chap12/lib/chap12.ex
@@ -10,9 +10,32 @@ defmodule Chap12 do
 
       iex> Chap12.hello()
       :world
-
   """
   def hello do
     :world
+  end
+
+  def upto(n) when n > 0, do: 1..n |> Enum.map(&caseFizzBuzz/1)
+
+  @doc """
+  iex> Chap12.caseFizzBuzz(15)
+  "FizzBuzz"
+
+  iex> Chap12.caseFizzBuzz(5)
+  "Buzz"
+
+  iex> Chap12.caseFizzBuzz(3)
+  "Fizz"
+
+  iex> Chap12.caseFizzBuzz(4)
+  4
+  """
+  def caseFizzBuzz(n) do
+    case {n, rem(n, 3), rem(n,5) } do
+      {_, 0, 0} -> "FizzBuzz"
+      {_, 0, _} -> "Fizz"
+      {_, _, 0} -> "Buzz"
+      {_, _, _} -> n
+    end
   end
 end
