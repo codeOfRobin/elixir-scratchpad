deda2d292dc9cc16a61b3b9cf924dbc3b3bc2e20
Robin Malhotra <robinm@twitter.com>
2020-02-10 22:45:33 +0000

    issues stuff

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -3,9 +3,6 @@ defmodule Issues.CLI do
   @moduledoc """
   Handle the command line parsing and the dispatch to the various functions that end up generating a table of the last _n_ issues in a github project
   """
-  def run(argv) do
-    parse_args(argv)
-  end
 
   @doc """
   `argv` can be -h or --help, which returns :help.
@@ -18,6 +15,7 @@ defmodule Issues.CLI do
     |> args_to_internal_representation()
   end
 
+  @spec args_to_internal_representation(any) :: :help | {any, any, any}
   def args_to_internal_representation([user, project, count]) do
     {user, project, String.to_integer(count)}
   end
