83953dfc433cc1a2bf1be44174444e640ee57eb7
Robin Malhotra <robinm@twitter.com>
2020-03-11 22:14:41 +0000

    Cleanup

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -1,4 +1,6 @@
 defmodule Issues.CLI do
+
+  import Issues.TableFormatter, only: [print_table_for_columns: 2]
   @default_count 4
   @moduledoc """
   Handle the command line parsing and the dispatch to the various functions that end up generating a table of the last _n_ issues in a github project
@@ -32,15 +34,7 @@ defmodule Issues.CLI do
     IO.puts("""
     usage: issues <user> <project> [ count | #{@default_count} ]
     """)
-
     System.halt(0)
-  end
-
-  def sort_into_descending_order(list_of_issues) do
-    list_of_issues
-    |> Enum.sort(fn i1, i2 ->
-      i1["created_at"] >= i2["created_at"]
-    end)
   end
 
   def process({user, project, issues_count}) do
@@ -48,8 +42,14 @@ defmodule Issues.CLI do
     |> decode_response()
     |> sort_into_descending_order()
     |> last(issues_count)
+    |> print_table_for_columns(["number", "created_at", "title"])
+  end
 
-    # |> print_table_for_columns(["number", "created_at", "title"])
+  def sort_into_descending_order(list_of_issues) do
+    list_of_issues
+    |> Enum.sort(fn i1, i2 ->
+      i1["created_at"] >= i2["created_at"]
+    end)
   end
 
   def last(list, count) do
@@ -60,7 +60,7 @@ defmodule Issues.CLI do
 
   def decode_response({:ok, body}), do: body
 
-  def decode_response({:error, error}) do
+  def decode_response({:error, _error}) do
     IO.puts("Error fetching from Github")
     System.halt(2)
   end
--- issues/lib/issues/table_formatter.ex
+++ issues/lib/issues/table_formatter.ex
@@ -1,14 +1,5 @@
 defmodule Issues.TableFormatter do
-  import Enum, only: [each: 2, map: 2, map_join: 3, max: 1, zip: 2]
-
-  @simple_test_data [
-    [c123456789: "r1 c1", c2: "r1 c2", c3: "r1 c3", c4: "r1+++c4"],
-    [c123456789: "r2 c1", c2: "r2 c2", c3: "r2 c3", c4: "r2 c4"],
-    [c123456789: "r3 c1", c2: "r3 c2", c3: "r3 c3", c4: "r3 c4"],
-    [c123456789: "r4 c1", c2: "r4++c2", c3: "r4 c3", c4: "r4 c4"]
-  ]
-
-  @headers [:c123456789, :c2, :c3, :c4]
+  import Enum, only: [map: 2, max: 1, zip: 2]
 
   def printable(str) when is_binary(str), do: str
   def printable(str), do: to_string(str)
@@ -68,11 +59,12 @@ defmodule Issues.TableFormatter do
     |> Enum.join("\n")
   end
   
-  def render_table(data, headers) do
-    widths = convert_to_columns(@simple_test_data, @headers)
+  def print_table_for_columns(data, headers) do
+    widths = convert_to_columns(data, headers)
               |> max_width()
-    render_header(@headers, widths)
+    render_header(headers, widths)
     <> "\n" <>
-    render_rows(@simple_test_data, @headers, widths)
+    render_rows(data, headers, widths)
+    <> "\n"
   end
 end
