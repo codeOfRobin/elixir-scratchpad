40e51bca07e50e5a50f8ef3fe19788e99a175ca0
Robin Malhotra <robinm@twitter.com>
2020-04-12 00:00:23 +0100

    Add process chapter

--- buggy/lib/buggy.ex
+++ buggy/lib/buggy.ex
@@ -6,11 +6,11 @@ defmodule Buggy do
   def parse_header(<<
         format::integer-16,
         tracks::integer-16,
-        division::integer-16
+        division::bits-16
       >>) do
-    IO.puts(format)
-    IO.puts(tracks)
-    IO.puts(division)
+    IO.puts("format: #{format}")
+    IO.puts("tracks: #{tracks}")
+    IO.puts("division: #{decode(division)}")
   end
 
   def decode(<<1::1, beats::15>>) do
